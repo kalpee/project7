6876d2d25c42c0118652f9f2670641603e82a90b
melchior99 <xhskwii@icloud.com>
2023-07-08 15:20:08 +0900

    .env.exampleの内容修正

--- src/resources/views/shop.blade.php
+++ src/resources/views/shop.blade.php
@@ -18,7 +18,7 @@
         <div class="">
             <div class="mx-auto" style="max-width:1200px">
                 <h1 style="color:#555555; text-align:center; font-size:1.2em; padding:24px 0px; font-weight:bold;">商品一覧</h1>
-                <a class="dropdown-item" href="{{ url('/order_history') }}">
+                <a class="dropdown-item" href="{{ url('/order_history') }}" style="width:7em;">
                     購入履歴
                 </a>
                 <div class="container">
